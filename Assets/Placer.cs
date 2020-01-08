e7ff88b6263973c23132b90d89d8fb6d91353f1d
itsbaptiste <baptiste.manusset@gmail.com>
2020-01-08 17:07:44 +0100

    yolo

---
+++ Assets/Placer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Placer : MonoBehaviour
+{
+    private Camera cam;
+    public GameObject carre;
+    Vector3 point;
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        point.y = 1;
+        carre.transform.position = point;
+    }
+
+    void OnGUI()
+    {
+        Event currentEvent = Event.current;
+        Vector2 mousePos = new Vector2();
+
+        // Get the mouse position from Event.
+        // Note that the y position from Event is inverted.
+        mousePos.x = currentEvent.mousePosition.x;
+        mousePos.y = cam.pixelHeight - currentEvent.mousePosition.y;
+
+        point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, cam.nearClipPlane));
+
+        GUILayout.BeginArea(new Rect(20, 20, 250, 120));
+        GUILayout.Label("Screen pixels: " + cam.pixelWidth + ":" + cam.pixelHeight);
+        GUILayout.Label("Mouse position: " + mousePos);
+        GUILayout.Label("World position: " + point.ToString("F3"));
+        GUILayout.EndArea();
+    }
+}
