bab6b95b97657689f1980ba77fc5ae451916f5af
BaptisteManusset <Baptiste.Manusset@gmail.com>
2020-01-26 12:33:53 +0100

    batiment test

--- Assets/MainWave.cs
+++ Assets/MainWave.cs
@@ -1,35 +1,35 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MainWave : MonoBehaviour
 {
-    public int waveCount = 5;
-    public int count;
-    public GameObject[] spawners;
-    public GameObject mob;
-    public Transform parent;
+  public int waveCount = 5;
+  public int count;
+  public GameObject[] spawners;
+  public GameObject[] mobs;
+  public Transform parent;
 
-    // Start is called before the first frame update
-    void Start()
+  // Start is called before the first frame update
+  void Start()
+  {
+    StartCoroutine(Waving());
+  }
+
+  IEnumerator Waving() // ~~¤_¤~~
+  {
+    Debug.Log("Debut de wave");
+    for (int i = 0; i < count; i++)
     {
-        StartCoroutine(Waving());
+      for (int y = 0; y < spawners.Length; y++)
+      {
+        Instantiate(mobs[Random.Range(0, mobs.Length)], spawners[y].transform.position, Quaternion.identity, parent);
+      }
+      yield return new WaitForSeconds(.5f);
     }
+    Debug.Log("Fin de wave");
 
-    IEnumerator Waving() // ~~¤_¤~~
-    {
-        Debug.Log("Debut de wave");
-        for (int i = 0; i < count; i++)
-        {
-            for (int y = 0; y < spawners.Length; y++)
-            {
-                Instantiate(mob, spawners[y].transform.position, Quaternion.identity, parent);
-            }
-            yield return new WaitForSeconds(.5f);
-        }
-        Debug.Log("Fin de wave");
-
-        yield return new WaitForSeconds(10);
-        Debug.Log("Fin d'interwave");
-    }
+    yield return new WaitForSeconds(10);
+    Debug.Log("Fin d'interwave");
+  }
 }
--- Assets/Scripts/Mind.cs
+++ Assets/Scripts/Mind.cs
@@ -7,88 +7,97 @@ using UnityEngine.UI;
 
 public class Mind : MonoBehaviour
 {
-    #region stats    
-    [BoxGroup("Stats")] [ProgressBar(null, 10)] public int health;
-    [BoxGroup("Stats")] public Image healthbar;
-    int maxHealth = 10;
-    bool alive = true;
+  #region stats    
+  [BoxGroup("Stats")] [ProgressBar(null, 10)] public int health;
+  [BoxGroup("Stats")] public Image healthbar;
+  int maxHealth = 10;
+  bool alive = true;
+  #endregion
+
+  NavMeshAgent meshAgent;
+  Canvas canvas;
+  Renderer cubeRenderer;
+
+  [BoxGroup("Deplacement")] public Transform destination;
+  [BoxGroup("Deplacement")] [Tag] public string tag;
+  //[BoxGroup("Valeur à la mort")] public int valeur = 10;
+
+  public FloatReference damageToTower;
+  [BoxGroup("Argent")] public FloatVariable argent;
+  [BoxGroup("Argent")] [Label("Valeur")] public FloatReference valeur;
+  [SerializeField] ThingRuntimeSet destinations;
+  public float speed = 3.5f;
+  public float radius = .4f;
+
+
+
+  void Start()
+  {
+    health = maxHealth;
+    #region definition de la destination
+    meshAgent = GetComponent<NavMeshAgent>();
+
+
+
+    meshAgent.speed = speed;
+    meshAgent.radius = radius;
+
+    //if (destinations == null)
+    //{
+    //    destinations = GameObject.FindGameObjectsWithTag("Destination");
+    //}
+    //destination = destinations.Items[Random.Range(0, destinations.Items.Count)].transform;
+    destination = GameObject.FindGameObjectWithTag(tag).transform;
+    if (destination == null) Debug.LogError("Impossible de trouver une destination");
+    meshAgent.SetDestination(destination.position);
     #endregion
-
-    NavMeshAgent MeshAgent;
-    Canvas canvas;
-    Renderer cubeRenderer;
-
-    [BoxGroup("Deplacement")] public Transform destination;
-    [BoxGroup("Deplacement")] [Tag] public string tag;
-    //[BoxGroup("Valeur à la mort")] public int valeur = 10;
-
-    public FloatReference damageToTower;
-    [BoxGroup("Argent")] public FloatVariable argent;
-    [BoxGroup("Argent")] [Label("Valeur")] public FloatReference valeur;
-    [SerializeField] ThingRuntimeSet destinations;
+    cubeRenderer = GetComponent<Renderer>();
+    canvas = GetComponentInChildren<Canvas>();
+    UpdateHealthBar();
 
 
-    void Start()
+
+  }
+  void Update()
+  {
+    canvas.transform.LookAt(MainLevel.instance.camera.transform);
+    NavMeshHit hit;
+    meshAgent.SamplePathPosition(-1, 0.0f, out hit);
+  }
+  public void Damage(int d)
+  {
+    if (alive)
     {
-        health = maxHealth;
-        #region definition de la destination
-        MeshAgent = GetComponent<NavMeshAgent>();
-        //if (destinations == null)
-        //{
-        //    destinations = GameObject.FindGameObjectsWithTag("Destination");
-        //}
-        //destination = destinations.Items[Random.Range(0, destinations.Items.Count)].transform;
-        destination = GameObject.FindGameObjectWithTag(tag).transform;
-        if (destination == null) Debug.LogError("Impossible de trouver une destination");
-        MeshAgent.SetDestination(destination.position);
-        #endregion
-        cubeRenderer = GetComponent<Renderer>();
-        canvas = GetComponentInChildren<Canvas>();
-        UpdateHealthBar();
-
-
-
+      health -= d;
     }
-    void Update()
+    if (health <= 0)
     {
-        canvas.transform.LookAt(MainLevel.instance.camera.transform);
-        NavMeshHit hit;
-        MeshAgent.SamplePathPosition(-1, 0.0f, out hit);
+      Dead();
     }
-    public void Damage(int d)
-    {
-        if (alive)
-        {
-            health -= d;
-        }
-        if (health <= 0)
-        {
-            Dead();
-        }
-        UpdateHealthBar();
-    }
+    UpdateHealthBar();
+  }
 
-    void Dead()
+  void Dead()
+  {
+    if (alive)
     {
-        if (alive)
-        {
-            health = 0;
-            alive = false;
-            MeshAgent.isStopped = true;
-            cubeRenderer.material.SetColor("_BaseColor", Color.red);
+      health = 0;
+      alive = false;
+      meshAgent.isStopped = true;
+      cubeRenderer.material.SetColor("_BaseColor", Color.red);
 
 
-            argent.ApplyChange(valeur);
-            Destroy(gameObject, 2);
-        }
+      argent.ApplyChange(valeur);
+      Destroy(gameObject, 2);
     }
-    public bool IsDead()
-    {
-        return !alive;
-    }
+  }
+  public bool IsDead()
+  {
+    return !alive;
+  }
 
-    private void UpdateHealthBar()
-    {
-        healthbar.fillAmount = (float)health / (float)maxHealth;
-    }
+  private void UpdateHealthBar()
+  {
+    healthbar.fillAmount = (float)health / (float)maxHealth;
+  }
 }
